ce2d1fcf426c232be4ecd3d285fd060f79865b28
Akramuzzaman-Raju <akraju012@gamil.com>
2023-11-22 00:23:21 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,8 @@ async function bootstrap() {
 
   app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
   app.enableCors();
-  // await app.listen(3001,()=>{
-  //   console.log('Your server is running on http://localhost:3001')
-  // });
-  await app.listen(process.env.PORT, '0.0.0.0', () => {
-    console.log(`Your server is running on http://${hostname()}:${process.env.PORT}`);
+  await app.listen(3001,()=>{
+    console.log('Your server is running on http://localhost:3001')
   });
 }
 bootstrap();
